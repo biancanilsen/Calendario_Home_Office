d9f164f8ee2e3ed5a58f67da24c56e67bd1d342c
biancanilsen <bianca.nilsen@paytrack.com.br>
2024-10-04 14:57:33 -0300

    Adiciona tela de cadastro de usuário

--- src/components/Login/LoginPage.tsx
+++ src/components/Login/LoginPage.tsx
@@ -1,41 +1,5 @@
-// import React, { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
-// import { Box, TextField, Button } from '@mui/material';
-
-// export default function LoginPage() {
-//   const [username, setUsername] = useState('');
-//   const [password, setPassword] = useState('');
-//   const navigate = useNavigate();
-
-//   const handleLogin = () => {
-//     // Lógica de autenticação aqui
-//     navigate('/calendar');
-//   };
-
-//   return (
-//     <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="100vh">
-//       <TextField
-//         label="Usuário"
-//         value={username}
-//         onChange={(e) => setUsername(e.target.value)}
-//         sx={{ mb: 2, width: '300px' }}
-//       />
-//       <TextField
-//         label="Senha"
-//         type="password"
-//         value={password}
-//         onChange={(e) => setPassword(e.target.value)}
-//         sx={{ mb: 2, width: '300px' }}
-//       />
-//       <Button variant="contained" color="primary" onClick={handleLogin}>
-//         Entrar
-//       </Button>
-//     </Box>
-//   );
-// }
-
 import { Box, Grid, Typography } from "@mui/material";
-import { Button, Input, Link, Spacer } from "@nextui-org/react";
+import { Button, Input, Link } from "@nextui-org/react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Usuario from "../../assets/Usuario.svg";
@@ -47,50 +11,70 @@ export default function LoginPage() {
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    // Lógica de autenticação aqui
     navigate("/calendar");
   };
 
   return (
     <Grid container sx={{ height: "100vh" }}>
-      {/* Metade da tela com o formulário de login */}
-      <Grid item xs={12} md={6} sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <Grid
+        item
+        xs={12}
+        md={6}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
         <Box sx={{ width: "80%", maxWidth: 400 }}>
-          <Typography variant="h4" fontWeight="bold" mb={2}>
+          <Typography variant="h4" fontWeight="540" sx={{ marginBottom: "20px", textAlign: "center" }}>
             Bem-vindo de volta!
           </Typography>
-          <Spacer y={1} />
-          <Input label="E-mail corporativo" placeholder="Digite seu e-mail" fullWidth onChange={(e) => setUsername(e.target.value)} value={username} required />
-          <Spacer y={1} />
-          <Input
-            label="Senha"
-            type="password"
-            placeholder="Digite sua senha"
-            fullWidth
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            required
-          />
-          <Spacer y={0.5} />
-          <Box sx={{ textAlign: "right" }}>
+
+          <Box sx={{ marginBottom: "15px" }}>
+            <Input
+              label="E-mail corporativo"
+              placeholder="Digite seu e-mail"
+              fullWidth
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+              required
+            />
+          </Box>
+
+          <Box sx={{ marginBottom: "30px" }}>
+            <Input
+              label="Senha"
+              type="password"
+              placeholder="Digite sua senha"
+              fullWidth
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              required
+            />
+          </Box>
+
+          <Box sx={{ textAlign: "right", width: "100%", marginBottom: "15px" }}>
             <Link href="#" underline="hover" color="secondary">
               Esqueceu sua senha?
             </Link>
           </Box>
-          <Spacer y={1} />
+
           <Button
             style={{
               backgroundColor: "#1a1a2e",
               color: "#fff",
               borderRadius: "20px",
               padding: "10px 0",
-              marginBottom: "10px",
+              marginBottom: "15px",
             }}
             onClick={handleLogin}
             fullWidth
           >
             Entrar
           </Button>
+
           <Button
             style={{
               backgroundColor: "#f5f5f5",
@@ -98,14 +82,15 @@ export default function LoginPage() {
               borderRadius: "20px",
               padding: "10px 0",
               border: "1px solid #ccc",
+              marginBottom: "40px",
             }}
             fullWidth
           >
             <img src={Google} alt="Google Icon" style={{ marginRight: "5px" }} />
             Entrar com Google
           </Button>
-          <Spacer y={1} />
-          <Box sx={{ textAlign: "center" }}>
+
+          <Box sx={{ textAlign: "center", width: "100%" }}>
             <Typography variant="body2">
               Não possui uma conta?{" "}
               <Link href="#" underline="hover" color="primary">
@@ -121,14 +106,14 @@ export default function LoginPage() {
         xs={12}
         md={6}
         sx={{
-          backgroundColor: "#1570EF",
+          background: "linear-gradient(211.4deg, #1570EF 0%, #3D5CA5 100%)",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
         <Box sx={{ padding: "40px" }}>
-          <img src={Usuario} alt="Login illustration" style={{ width: "100%", maxWidth: "500px" }} />
+          <img src={Usuario} alt="Login illustration" style={{ width: "100%", maxWidth: "600px" }} />
         </Box>
       </Grid>
     </Grid>
--- src/components/Register/RegisterPage.tsx
+++ src/components/Register/RegisterPage.tsx
@@ -1,41 +1,107 @@
-import React, { useState } from 'react';
-import { Box, TextField, Button } from '@mui/material';
+import { Box, Button, Grid, Typography } from "@mui/material";
+import { Input, Link } from "@nextui-org/react";
+import { useState } from "react";
+import Usuario from "../../assets/Usuario.svg";
 
 export default function RegisterPage() {
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [empresa, setEmpresa] = useState("");
+  const [squad, setSquad] = useState("");
 
   const handleRegister = () => {
-    // Lógica de registro aqui
-    console.log('Cadastro efetuado:', { username, email, password });
+    console.log("Cadastro efetuado:", { username, email, password });
   };
 
   return (
-    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" height="100vh">
-      <TextField
-        label="Usuário"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        sx={{ mb: 2, width: '300px' }}
-      />
-      <TextField
-        label="Email"
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        sx={{ mb: 2, width: '300px' }}
-      />
-      <TextField
-        label="Senha"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        sx={{ mb: 2, width: '300px' }}
-      />
-      <Button variant="contained" color="primary" onClick={handleRegister}>
-        Cadastrar
-      </Button>
-    </Box>
+    <Grid container sx={{ height: "100vh" }}>
+      <Grid
+        item
+        xs={12}
+        md={6}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Box sx={{ width: "80%", maxWidth: 400 }}>
+          <Typography variant="h4" fontWeight="540" sx={{ marginBottom: "10px", textAlign: "center", color: "#101828" }}>
+            Boas vindas ao InOut!
+          </Typography>
+
+          <Typography variant="subtitle1" sx={{ marginBottom: "30px", textAlign: "center", color: "#475467" }}>
+            Crie sua conta gratuitamente.
+          </Typography>
+
+          <Box sx={{ marginBottom: "30px" }}>
+            <Input label="Nome" placeholder="Digite seu nome" fullWidth onChange={(e) => setUsername(e.target.value)} value={username} required />
+          </Box>
+
+          <Box sx={{ marginBottom: "30px" }}>
+            <Input label="E-mail corporativo" placeholder="Digite sua e-mail" fullWidth onChange={(e) => setEmail(e.target.value)} value={password} required />
+          </Box>
+
+          <Box sx={{ marginBottom: "30px" }}>
+            <Input label="Senha" type="password" placeholder="********" fullWidth onChange={(e) => setPassword(e.target.value)} value={password} required />
+          </Box>
+
+          <Box sx={{ marginBottom: "30px" }}>
+            <Input
+              label="Empresa"
+              placeholder="Informe o nome de sua empresa"
+              fullWidth
+              onChange={(e) => setEmpresa(e.target.value)}
+              value={password}
+              required
+            />
+          </Box>
+
+          <Box sx={{ marginBottom: "30px" }}>
+            <Input label="Squad" placeholder="Selecione sua squad" fullWidth onChange={(e) => setSquad(e.target.value)} value={password} required />
+          </Box>
+
+          <Button
+            style={{
+              backgroundColor: "#1a1a2e",
+              color: "#fff",
+              borderRadius: "20px",
+              padding: "10px 0",
+              marginBottom: "15px",
+            }}
+            fullWidth
+          >
+            Criar conta
+          </Button>
+
+          <Box sx={{ textAlign: "center", width: "100%" }}>
+            <Typography variant="body2" color={"#475467"}>
+              Já possui uma conta?{" "}
+              <Link href="/" underline="hover" color="primary">
+                Entrar
+              </Link>
+            </Typography>
+          </Box>
+        </Box>
+      </Grid>
+
+      <Grid
+        item
+        xs={12}
+        md={6}
+        sx={{
+          background: "linear-gradient(211.4deg, #1570EF 0%, #3D5CA5 100%)",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box sx={{ padding: "40px" }}>
+          <img src={Usuario} alt="Login illustration" style={{ width: "100%", maxWidth: "600px" }} />
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
