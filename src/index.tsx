43cea17398f4a0dfc0c62c2ba0d0b6d04f04f5c7
biancanilsen <bianca.nilsen@paytrack.com.br>
2024-10-07 20:54:10 -0300

    Adiciona GoogleOAuthProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { NextUIProvider } from "@nextui-org/react";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -8,9 +9,11 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
+    <GoogleOAuthProvider clientId="646276690504-mvc6la11av50gpubq83m0r45h1cqkp6k.apps.googleusercontent.com">
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
