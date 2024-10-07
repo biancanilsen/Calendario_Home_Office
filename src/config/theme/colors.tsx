eaebd98f47f0dc26a32728e8a5aafbe602907535
biancanilsen <bianca.nilsen@paytrack.com.br>
2024-10-07 20:57:23 -0300

    cria theme para usar as cores universais no projeto

--- src/config/theme/colors.tsx
+++ src/config/theme/colors.tsx
@@ -3,8 +3,10 @@ export const colors = {
   black: "#101828",
   primary: "#1a1a2e",
   white: "#f5f5f5",
-  success: "#4caf50",
+  success: "#079455",
   greyC200: "#ccc",
-  blue: "1570EF",
-  blueC200: "3D5CA5",
+  blue: "#1570EF",
+  blueC200: "#3D5CA5",
+  bgPrimary: "#FDFDFD",
+  error: "#D92D20",
 };
